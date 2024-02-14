68e46fe11cc2b72dd62e6bc619d53c5b6eaf9384
GiuliaBortone <giulia.bortone@mail.polimi.it>
2024-02-14 15:09:21 +0100

    can now check if solution matrix is valid solution for original incomplete matrix

--- src/SudokuSolutionChecker.java
+++ src/SudokuSolutionChecker.java
@@ -6,6 +6,19 @@ public class SudokuSolutionChecker {
     }
 
     public boolean isAValidSolution(Matrix possibleSolution) {
-        return false;
+        MatrixValidityChecker checker = new MatrixValidityChecker(possibleSolution);
+
+        return checker.isMatrixValid() && areOriginalNumbersStillPresentIn(possibleSolution);
+    }
+
+    private boolean areOriginalNumbersStillPresentIn(Matrix possibleSolution) {
+        for (int row = 0; row < originalMatrix.rows(); row++) {
+            for (int column = 0; column < originalMatrix.columns(); column++) {
+                if (originalMatrix.array()[row][column] != 0 && originalMatrix.array()[row][column] != possibleSolution.array()[row][column]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
